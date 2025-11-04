c556c3031b9d674263fdd62e9a20b463007ad3aa
Omer H <omer.h1910@gmail.com>
2025-11-04 18:41:43 +0200

    finished calculating pi program

--- CalcPi.java
+++ CalcPi.java
@@ -2,5 +2,16 @@
 public class CalcPi {
 	public static void main(String [] args) { 
 	    // Replace this comment with your code
+		int numberOfTerms = Integer.parseInt(args[0]);
+		double sum = 1;
+		double divisor = 3;
+		int multiplier = -1;
+		for(int i = 0; i < numberOfTerms; i++){
+			sum += (1/divisor) * multiplier;
+			multiplier *= -1;
+			divisor += 2;
+		}
+		System.out.println("pi according to Java: " + Math.PI);
+		System.out.println("pi, approximated: " + sum * 4);
 	}
 }
