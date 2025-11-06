72c11856f52b9e14591e47e2faf38de2c1df2ecc
Omer H <omer.h1910@gmail.com>
2025-11-06 11:05:32 +0200

    fixed empty space when facing an 'a'

--- Cheers.java
+++ Cheers.java
@@ -16,7 +16,7 @@ public class Cheers {
                 String letter = "";
                 for(int i = 0; i < word.length(); i++){
                         letter = (word.charAt(i) + "").toUpperCase();
-                        shout = String.format("Give me a%s %s: %s!", (letters.contains(letter) ? "n" : ""), letter, letter);
+                        shout = String.format("Give me a%s %s: %s!", (letters.contains(letter) ? "n" : " "), letter, letter);
                         System.out.println(shout);
                 }
                 System.out.println("What does that spell?");
