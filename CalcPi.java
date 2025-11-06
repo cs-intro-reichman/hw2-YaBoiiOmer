d852a0359d17f412c5b2b03f6a855791e1159b0c
Omer H <omer.h1910@gmail.com>
2025-11-06 11:24:12 +0200

    fixed first term

--- CalcPi.java
+++ CalcPi.java
@@ -3,9 +3,9 @@ public class CalcPi {
 	public static void main(String [] args) { 
 	    // Replace this comment with your code
 		int numberOfTerms = Integer.parseInt(args[0]);
-		double sum = 1;
-		double divisor = 3;
-		int multiplier = -1;
+		double sum = 0;
+		double divisor = 1;
+		int multiplier = 1;
 		for(int i = 0; i < numberOfTerms; i++){
 			sum += (1/divisor) * multiplier;
 			multiplier *= -1;
